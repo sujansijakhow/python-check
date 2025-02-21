712f943877ee4d84455415a58af492f8bc0d90f5
sujansijakhow <sujansijakhow@gmail.com>
2025-02-21 08:11:04 +0545

    Operator file added

---
+++ operators.py
@@ -0,0 +1,24 @@
+
+
+x = 5;    # Assignment Operator
+
+if x == 5:  # Comparison Operator
+    print("Value Matched")
+
+a = 5;
+b = 5;
+
+print(a + b)      # Airthmetic Operator
+
+
+s = 4;
+u = 4.0
+
+if s is u:        # identity operator
+    print("Identical")
+else:
+    print("Not identical") 
+
+
+# if s == u:
+#     print("Matched")
